05b99fcf64b591b6390329e0329e93b93a5b0342
Majid Ansari <MAJID_ANSARI24@manavrachna.net>
2026-03-13 23:59:41 +0530

    Analytic add

--- controllers/shortnerController.js
+++ controllers/shortnerController.js
@@ -1,12 +1,17 @@
 const QRCode = require("qrcode");
 const { nanoid } = require("nanoid");
 const pgdb = require("../config/db");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const UAParser = require("ua-parser-js");
+const geoip = require("geoip-lite");
 
 async function urlredirect(req, res) {
   try {
     const { code } = req.params;
-    const { password } = req.body;
+    const password = req.body?.password;
+
+    const geo = geoip.lookup(req.ip);
+    const parser = new UAParser(req.headers["user-agent"]);
 
     const query = `
       UPDATE shorten
@@ -21,14 +26,35 @@ async function urlredirect(req, res) {
       return res.status(404).json({ error: "Short URL not found" });
     }
 
+    const insertClick = `
+      INSERT INTO clicks
+      (short_code, ip_address, country, browser, device, os, referrer)
+      VALUES ($1,$2,$3,$4,$5,$6,$7)
+      RETURNING id
+    `;
+
+    await pgdb.query(insertClick, [
+      code,
+      req.ip,
+      geo?.country || "Unknown",
+      parser.getBrowser().name,
+      parser.getDevice().type || "desktop",
+      parser.getOS().name,
+      req.get("Referrer") || "direct",
+    ]);
+
     const { original, pass } = rows[0];
 
     if (pass) {
       const matching = await bcrypt.compare(password, pass);
+
       if (!matching) {
-        return res.status(403).json({ error: "Password not correct or not provided" });
+        return res
+          .status(403)
+          .json({ error: "Password not correct or not provided" });
       }
-      return res.json(original);
+
+      return res.json({ url: original });
     }
 
     return res.redirect(original);
@@ -37,7 +63,6 @@ async function urlredirect(req, res) {
     return res.status(500).json({ error: "Server error" });
   }
 }
-
 
 async function shorten(req, res) {
   try {
@@ -52,10 +77,15 @@ async function shorten(req, res) {
     } else {
       try {
         const { url, custom_short, email, password } = req.body;
-        const hashedpass = await bcrypt.hash(password,10);
+        const hashedpass = await bcrypt.hash(password, 10);
         const shortenedUrl = custom_short || nanoid(5);
         const query = `insert into shorten(original,shortcode,created_by,pass) values($1,$2,$3,$4) RETURNING id,original,shortcode,created_by`;
-        const { rows } = await pgdb.query(query, [url, shortenedUrl, email,hashedpass]);
+        const { rows } = await pgdb.query(query, [
+          url,
+          shortenedUrl,
+          email,
+          hashedpass,
+        ]);
         return res.json({
           shortenedUrl,
         });
